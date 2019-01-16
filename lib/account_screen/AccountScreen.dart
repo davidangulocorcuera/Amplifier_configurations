9c2e979099de38a695a1e250416b763e479c5584
David <davidangulococruera@gmail.com>
2019-01-16 14:38:37 +0100

    creating screens

---
+++ lib/account_screen/AccountScreen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+
+class AcountScreen extends StatefulWidget {
+  AcountScreen({Key key, this.title}) : super(key: key);
+  final String title;
+  @override
+  State<StatefulWidget> createState() {
+  }
+}
+class _AccountScreenState extends State<AcountScreen> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
