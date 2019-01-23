69f2a72de4f042ff05282d7d04dacbf28364350f
David <davidangulococruera@gmail.com>
2019-01-23 14:33:01 +0100

    login y registro hecho

--- lib/register_screen/RegisterScreen.dart
+++ lib/register_screen/RegisterScreen.dart
@@ -35,139 +35,143 @@ class _RegisterScreenState extends State<RegisterScreen>
   Widget build(BuildContext context) {
     return Scaffold(
       key: _scaffoldState,
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        backgroundColor: Colors.black38,
-      ),
       backgroundColor: Colors.black,
-      body:Stack(
-              fit: StackFit.expand,
-              children: <Widget>[
-                Image(
-                  image: AssetImage("assets/guitarist.jpg"),
-                  fit: BoxFit.cover,
-                  color: Colors.black87,
-                  colorBlendMode: BlendMode.darken,
-                ),
-                SingleChildScrollView(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      FlutterLogo(
-                        size: 100.0,
-                      ),
-                      Form(
-                        key: _formKey,
-                        child: Theme(
-                          data: ThemeData(
-                              brightness: Brightness.dark,
-                              primarySwatch: Colors.teal,
-                              inputDecorationTheme: InputDecorationTheme(
-                                  labelStyle: TextStyle(
-                                      color: Colors.white70, fontSize: 20.0))),
-                          child: Container(
-                            padding: const EdgeInsets.all(40.0),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: <Widget>[
-                                TextFormField(
-                                  decoration: InputDecoration(
-                                    labelText: "Enter E-mail",
-                                  ),
-                                  keyboardType: TextInputType.emailAddress,
-                                  onSaved: (value) => _email = value,
-                                  validator: (value) {
-                                    if (!_presenter.validateEmail(value)) {
-                                      return 'Incorrect e-mail';
-                                    }
-                                  },
-                                  enabled: !_isLoading,
-                                ),
-                                TextFormField(
-                                  decoration: InputDecoration(
-                                    labelText: "Enter Password",
-                                  ),
-                                  keyboardType: TextInputType.text,
-                                  onSaved: (value) => _password = value,
-                                  validator: (value) {
-                                    if (!_presenter.validatePassword(value)) {
-                                      return 'The password must be 6 characters long or more.';
-                                    }
-                                  },
-                                  enabled: !_isLoading,
-                                  obscureText: true,                                ),
-                                Padding(
-                                  padding: const EdgeInsets.only(top: 40.0),
-                                ),
-                                _isLoading
-                                    ? CircularProgressIndicator()
-                                    : MaterialButton(
-                                  height: 40.0,
-                                  minWidth: 60.0,
-                                  color: Colors.green,
-                                  textColor: Colors.white,
-                                  child: Text("Sign up!"),
-                                  onPressed: () async {
-                                    if (_formKey.currentState.validate()) {
-                                      _formKey.currentState.save();
-                                      _presenter.registerClicked(
-                                          _email, _password);
-                                    }
-                                  },
-                                  splashColor: Colors.blueAccent,
-                                ),
-                                Padding(
-                                    padding: const EdgeInsets.only(top: 10.0)),
-                                FlatButton(
-                                  onPressed: () {
-                                    Navigator.pop(context);
-                                  },
-                                  splashColor: Colors.blueAccent,
-                                  child: Text(
-                                    "Sign in",
-                                    style: TextStyle(
-                                      decoration: TextDecoration.underline,
-                                      decorationColor: Colors.white70,
-                                      color: Colors.white70,
-                                      fontSize: 15.0,
+      body:SafeArea(
+        child: Stack(
+                fit: StackFit.expand,
+                children: <Widget>[
+                  Image(
+                    image: AssetImage("assets/guitarist.jpg"),
+                    fit: BoxFit.cover,
+                    color: Colors.black87,
+                    colorBlendMode: BlendMode.darken,
+                  ),
+                  SingleChildScrollView(
+                    child: Padding(
+                      padding: const EdgeInsets.only(top : 50.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          FlutterLogo(
+                            size: 100.0,
+                          ),
+                          Form(
+                            key: _formKey,
+                            child: Theme(
+                              data: ThemeData(
+                                  brightness: Brightness.dark,
+                                  primarySwatch: Colors.teal,
+                                  inputDecorationTheme: InputDecorationTheme(
+                                      labelStyle: TextStyle(
+                                          color: Colors.white70, fontSize: 20.0))),
+                              child: Container(
+                                padding: const EdgeInsets.all(40.0),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: <Widget>[
+                                    TextFormField(
+                                      decoration: InputDecoration(
+                                        labelText: "Enter E-mail",
+                                      ),
+                                      keyboardType: TextInputType.emailAddress,
+                                      onSaved: (value) => _email = value,
+                                      validator: (value) {
+                                        if (!_presenter.validateEmail(value)) {
+                                          return 'Incorrect e-mail';
+                                        }
+                                      },
+                                      enabled: !_isLoading,
                                     ),
-                                  ),
-                                ),
-                                Padding(
-                                    padding: const EdgeInsets.only(top: 20.0)),
-                                new CheckboxListTile(
-                                  value: _valueCheck,
-                                  onChanged: _valueCheckChanged,
-                                  title:
-                                      Text('I agree to the terms of service'),
-                                  controlAffinity:
-                                      ListTileControlAffinity.leading,
-                                  activeColor: Colors.teal,
-                                ),
-                                FlatButton(
-                                  onPressed: () {},
-                                  splashColor: Colors.blueAccent,
-                                  child: Text(
-                                    "Terms of service",
-                                    style: TextStyle(
-                                      decoration: TextDecoration.underline,
-                                      decorationColor: Colors.white70,
-                                      color: Colors.white70,
-                                      fontSize: 15.0,
+                                    TextFormField(
+                                      decoration: InputDecoration(
+                                        labelText: "Enter Password",
+                                      ),
+                                      keyboardType: TextInputType.text,
+                                      onSaved: (value) => _password = value,
+                                      validator: (value) {
+                                        if (!_presenter.validatePassword(value)) {
+                                          return 'The password must be 6 characters long or more.';
+                                        }
+                                      },
+                                      enabled: !_isLoading,
+                                      obscureText: true,                                ),
+                                    Padding(
+                                      padding: const EdgeInsets.only(top: 40.0),
                                     ),
-                                  ),
+                                    _isLoading
+                                        ? CircularProgressIndicator()
+                                        : MaterialButton(
+                                      height: 40.0,
+                                      minWidth: 60.0,
+                                      color: Colors.green,
+                                      textColor: Colors.white,
+                                      child: Text("Sign up!"),
+                                      onPressed: () async {
+                                        if(!_valueCheck){
+                                          showTermsError();
+                                        }
+                                        else if (_formKey.currentState.validate()) {
+                                          _formKey.currentState.save();
+                                          _presenter.registerClicked(
+                                              _email, _password);
+                                        }
+                                      },
+                                      splashColor: Colors.blueAccent,
+                                    ),
+                                    Padding(
+                                        padding: const EdgeInsets.only(top: 10.0)),
+                                    FlatButton(
+                                      onPressed: () {
+                                        Navigator.pop(context);
+                                      },
+                                      splashColor: Colors.blueAccent,
+                                      child: Text(
+                                        "Sign in",
+                                        style: TextStyle(
+                                          decoration: TextDecoration.underline,
+                                          decorationColor: Colors.white70,
+                                          color: Colors.white70,
+                                          fontSize: 15.0,
+                                        ),
+                                      ),
+                                    ),
+                                    Padding(
+                                        padding: const EdgeInsets.only(top: 20.0)),
+                                    new CheckboxListTile(
+                                      value: _valueCheck,
+                                      onChanged: _valueCheckChanged,
+                                      title:
+                                          Text('I agree to the terms of service'),
+                                      controlAffinity:
+                                          ListTileControlAffinity.leading,
+                                      activeColor: Colors.teal,
+                                    ),
+                                    FlatButton(
+                                      onPressed: () {},
+                                      splashColor: Colors.blueAccent,
+                                      child: Text(
+                                        "Terms of service",
+                                        style: TextStyle(
+                                          decoration: TextDecoration.underline,
+                                          decorationColor: Colors.white70,
+                                          color: Colors.white70,
+                                          fontSize: 15.0,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
                                 ),
-                              ],
+                              ),
                             ),
                           ),
-                        ),
+                          // Find the Scaffold in the Widget tree and use it to show a SnackBar!
+                        ],
                       ),
-                      // Find the Scaffold in the Widget tree and use it to show a SnackBar!
-                    ],
+                    ),
                   ),
-                ),
-              ],
-            ),
+                ],
+              ),
+      ),
     );
   }
 
@@ -197,7 +201,15 @@ class _RegisterScreenState extends State<RegisterScreen>
   showRegisterError() {
     _scaffoldState.currentState.showSnackBar(SnackBar(
       content: Text("The user already exists"),
-      backgroundColor: Colors.teal,
+      backgroundColor: Colors.red,
+      duration: Duration(seconds: 1),
+    ));
+  }
+  @override
+  showTermsError() {
+    _scaffoldState.currentState.showSnackBar(SnackBar(
+      content: Text("May accept the terms of service"),
+      backgroundColor: Colors.red,
       duration: Duration(seconds: 1),
     ));
   }
